1205fbeb4f893681b35c4964350e52e70bf1aec0
luizavramos <luizavramos30@gmail.com>
2023-09-03 11:22:20 -0300

    criacao login

--- app/src/main/java/br/com/fiap/startup/screen/LoginScreen.kt
+++ app/src/main/java/br/com/fiap/startup/screen/LoginScreen.kt
@@ -1,6 +1,11 @@
 package br.com.fiap.startup.screen
 
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -29,13 +34,31 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.Snackbar
 import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import br.com.fiap.startup.R
+import br.com.fiap.startup.LoginManager
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @Composable
 fun LoginScreen(navController: NavController) {
+
+
+
     var loginState = remember {
         mutableStateOf("")
     }
@@ -44,7 +67,11 @@ fun LoginScreen(navController: NavController) {
         mutableStateOf("")
     }
 
-    Column {
+
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.Transparent)) {
+        ImageInBox()
         LoginForm(
             login = loginState.value,
             senha = senhaState.value,
@@ -61,6 +88,22 @@ fun LoginScreen(navController: NavController) {
         }
     }
 }
+
+@Composable
+fun ImageInBox() {
+    Box(
+        modifier = Modifier.fillMaxWidth() // Largura igual à tela
+            .height(100.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        // Carregue uma imagem de recurso
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = null, // Descrição opcional para acessibilidade
+            modifier = Modifier.fillMaxWidth().fillMaxHeight()
+        )
+    }
+}
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginForm(
@@ -71,16 +114,16 @@ fun LoginForm(
     navController: NavController
 ) {
 
-    Column(
-        modifier = Modifier.padding(16.dp)
+    val loginManager = LoginManager(LocalContext.current)
+
+    Column(modifier = Modifier.padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
             text = "Login",
             fontSize = 24.sp,
             fontWeight = FontWeight.Bold,
-            color = Color(
-                0xFFE91E63
-            )
+            color = Color(0xFF02112E)
         )
         Spacer(modifier = Modifier.height(8.dp))
         OutlinedTextField(
@@ -105,11 +148,21 @@ fun LoginForm(
             },
             keyboardOptions = KeyboardOptions(
                 keyboardType = KeyboardType.Password
-            )
+            ),
+            visualTransformation = PasswordVisualTransformation()
         )
         Spacer(modifier = Modifier.height(16.dp))
         Button(
-            onClick = { navController.navigate("busca") },
+            onClick = {
+                val loginEValido = loginManager.realizarLogin(login, senha) // Use o LoginManager para verificar o login
+                if (loginEValido) {
+                    // Login bem-sucedido, navegue para a próxima tela
+                    navController.navigate("busca")
+                } else {
+
+
+                }
+          },
             modifier = Modifier.fillMaxWidth()
         ) {
             Text(
@@ -117,8 +170,12 @@ fun LoginForm(
                 modifier = Modifier.padding(8.dp)
             )
         }
+
+
     }
 }
+
+
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
